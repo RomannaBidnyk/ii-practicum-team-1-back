dd18f2629e89360c63d873b3caa4343ed22580bf
Ligo_code <rigatovt@gmail.com>
2025-04-01 10:28:03 -0400

    app.js refactor

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
-app.use(express.static('public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 // routes
