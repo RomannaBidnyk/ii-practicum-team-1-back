e32a275319040b03086c13f39eb60ff76a7e9386
RomannaBidnyk <romanna.bidnyk@gmail.com>
2025-04-14 22:40:18 -0400

    change format of images to jpg

--- src/controllers/cloudinaryController.js
+++ src/controllers/cloudinaryController.js
@@ -14,6 +14,7 @@ const uploadImage = async (req, res) => {
     // Upload to Cloudinary
     const result = await cloudinary.uploader.upload(base64Image, {
       folder: "kindnet", //  folder in Cloudinary - e.g. use test_uploads for testing
+      format: "jpg",
     });
 
     res.json({
