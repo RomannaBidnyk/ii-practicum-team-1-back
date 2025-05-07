c5c53a4226a97bfe274c9e00319612564bfa1502
RomannaBidnyk <romanna.bidnyk@gmail.com>
2025-05-06 23:00:31 -0400

    update user info api

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
-const { getUserInfo } = require("../controllers/userController");
+const {
+  getUserInfo,
+  updateUserInfo,
+} = require("../controllers/userController");
+const {
+  imageUploadPipeline,
+} = require("../middleware/imageUploadAndDeletePipeline");
 
 router.get("/info", authMiddleware, getUserInfo);
+router.put("/info", authMiddleware, imageUploadPipeline, updateUserInfo);
 
 module.exports = router;
